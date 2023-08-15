b10c07582f2e74f873e7743fd9c7deea517e40df
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-08-15 12:02:57 +0000

    binary search

---
+++ Primeagen Course/Search Sort/binarySearch.ts
@@ -0,0 +1,25 @@
+
+
+function binarySearch(haystack: number[], needle: number) {
+
+    let left = 0;
+    let right = haystack.length - 1;
+
+    while (left <= right) {
+
+        let mid = (left + right) >> 2;
+        if (haystack[mid] === needle) {
+            return mid;
+        }
+
+        else if (haystack[mid] < needle) {
+            left = mid + 1;
+        }
+
+        right = mid - 1;
+
+    }
+
+    return left;
+
+}
