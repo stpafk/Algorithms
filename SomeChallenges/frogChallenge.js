49055427a032f6685dd948f3bfd0b8a01258a052
stpafk <stephan.weigert@hotmail.com>
2023-09-24 23:10:27 -0300

    correct

--- SomeChallenges/frogChallenge.js
+++ SomeChallenges/frogChallenge.js
@@ -1,28 +1,14 @@
 function solution(a) {
+	var c = 0, i = 0;
 
-    let jumps = 0;
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] > 0) {
-            jumps++;
-            if (i + a[i] >= a.lenght) {
-                return jumps;
-            }
-            i += a[i];
-        }
-
-        if (a[i] < 0) {
-            jumps++;
-            if (i - a[i] <= -1) {
-                return jumps;
-            }
-            i -= a[i];
-        }
+	while (typeof a[i] !== 'undefined') {
+    c++;
+    i += a[i];
+    
+    if (c > a.length) {
+    	return -1;
     }
-
-    if (jumps === 0) {
-        return -1;
-    }
-    return jumps;
   }
 
-solution([1, 2, 2, -1])
+  return a.length ? c : -1;
+}
