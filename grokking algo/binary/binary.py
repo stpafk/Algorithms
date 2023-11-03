d3cf69d29e38bedd52b4ed54c824a5d9c091490f
stpafk <stephan.weigert@hotmail.com>
2023-11-03 15:05:45 -0300

    fix binary

--- grokking algo/binary/binary.py
+++ grokking algo/binary/binary.py
@@ -7,22 +7,28 @@ def binary_search(sortable, item):
 
     low = 0
     high = len(sortable) - 1
+    count = 0
 
     while low <= high:
-        mid = (low + high) / 2
+    
+        mid = (low + high) // 2
         guess = sortable[mid]
 
         if guess == item:
-            return mid
+            count+= 1
+            return mid, count
         
         if guess < item:
+            count+= 1
             low = mid + 1
         
-        if guess > item:
+        else:
+            count+= 1
             high = mid - 1
 
-    return None
+    return None, count
 
-print(binary_search([1, 3, 5, 7, 8], 4))
+print(binary_search([1, 3, 5, 7, 8], 3))
+print(binary_search([i for i in range(20000)], 1))
         
 
